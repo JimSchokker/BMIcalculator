2e757aa184dfe9c60624e077125f73f64772ca42
JimSchokker <jimschokker12@gmail.com>
2020-10-01 14:35:02 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace BMIcalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBerekenen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLengte = new System.Windows.Forms.TextBox();
+            this.txtLeeftijd = new System.Windows.Forms.TextBox();
+            this.txtGewicht = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblAdvies = new System.Windows.Forms.Label();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBerekenen
+            // 
+            this.btnBerekenen.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBerekenen.Location = new System.Drawing.Point(259, 237);
+            this.btnBerekenen.Name = "btnBerekenen";
+            this.btnBerekenen.Size = new System.Drawing.Size(278, 102);
+            this.btnBerekenen.TabIndex = 0;
+            this.btnBerekenen.Text = "BMI Berekenen";
+            this.btnBerekenen.UseVisualStyleBackColor = true;
+            this.btnBerekenen.Click += new System.EventHandler(this.btnBerekenen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(286, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "BMI Calulator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gewicht in kg:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Leeftijd:";
+            // 
+            // txtLengte
+            // 
+            this.txtLengte.Location = new System.Drawing.Point(311, 64);
+            this.txtLengte.Name = "txtLengte";
+            this.txtLengte.Size = new System.Drawing.Size(168, 22);
+            this.txtLengte.TabIndex = 4;
+            // 
+            // txtLeeftijd
+            // 
+            this.txtLeeftijd.Location = new System.Drawing.Point(311, 155);
+            this.txtLeeftijd.Name = "txtLeeftijd";
+            this.txtLeeftijd.Size = new System.Drawing.Size(168, 22);
+            this.txtLeeftijd.TabIndex = 5;
+            // 
+            // txtGewicht
+            // 
+            this.txtGewicht.Location = new System.Drawing.Point(311, 111);
+            this.txtGewicht.Name = "txtGewicht";
+            this.txtGewicht.Size = new System.Drawing.Size(168, 22);
+            this.txtGewicht.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 377);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "BMI:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 420);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Advies:";
+            // 
+            // lblAdvies
+            // 
+            this.lblAdvies.AutoSize = true;
+            this.lblAdvies.Location = new System.Drawing.Point(361, 420);
+            this.lblAdvies.Name = "lblAdvies";
+            this.lblAdvies.Size = new System.Drawing.Size(46, 17);
+            this.lblAdvies.TabIndex = 10;
+            this.lblAdvies.Text = "label7";
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Location = new System.Drawing.Point(361, 377);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(46, 17);
+            this.lblBMI.TabIndex = 11;
+            this.lblBMI.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(166, 64);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(81, 17);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "lengte in m:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.lblAdvies);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtGewicht);
+            this.Controls.Add(this.txtLeeftijd);
+            this.Controls.Add(this.txtLengte);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBerekenen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBerekenen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLengte;
+        private System.Windows.Forms.TextBox txtLeeftijd;
+        private System.Windows.Forms.TextBox txtGewicht;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblAdvies;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIcalculator
+{
+    public partial class Form1  :Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBerekenen_Click(object sender, EventArgs e)
+        {
+            {
+                float gewicht = float.Parse(txtGewicht.Text);
+                float lengte = float.Parse(txtLengte.Text);
+                float leeftijd = float.Parse(txtLeeftijd.Text);
+
+                float BMI = gewicht / (lengte * lengte); ;
+                string advies = "";
+
+                if (BMI < 18.5)
+                {
+                    advies = "Je hebt ondergewicht.";
+                }
+                else if (BMI > 18.5 && BMI < 25)
+                {
+                    advies = "je hebt een normaal gewicht.";
+                }
+                else if (BMI > 25)
+                {
+                    advies = "je hebt overgewicht.";
+                }
+                
+
+                lblBMI.Text = BMI.ToString();
+                lblAdvies.Text = advies;
+            }
+        }
+    }
+}
